191f95945b91bb96ae6b57ee3d9971144622f9bc
JKS9 <juzans.etienne@gmail.com>
2022-10-17 20:05:49 +0200

    add routing And Fake Authentication

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,10 +1,66 @@
+import {useEffect, useState} from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import NotFoundViews from './notFound/notFoundViews';
+import SignupViews from './signup/signupViews';
+import LoginViews from './login/loginViews';
+import HomeViews from './home/homeViews';
+
 import '../styles/App.css';
 
 const App = () => {
+  const [logging, setLogging] = useState<boolean>(false);
+
+  useEffect(() => {
+    let authToken = sessionStorage.getItem('Auth Token');
+
+    if (authToken) {
+      setLogging(true);
+    } else {
+      setLogging(false);
+    }
+  }, [logging]);
+
+  const handlerLogout = () => {
+    sessionStorage.removeItem('Auth Token');
+    setLogging(false);
+  };
+
+  const handlerLogin = () => {
+    setLogging(true);
+  };
+
   return (
-    <div className='App'>
-      <h1>Etienne</h1>
-    </div>
+    <>
+      {logging ? (
+        <Routes>
+          <Route
+            index
+            path='/'
+            element={<HomeViews logout={handlerLogout} />}
+          />
+          <Route
+            path='/*'
+            element={<NotFoundViews />}
+          />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route
+            path='/'
+            element={<SignupViews />}
+          />
+          <Route
+            path='/login'
+            element={<LoginViews login={handlerLogin} />}
+          />
+          <Route
+            path='/*'
+            element={<NotFoundViews />}
+          />
+        </Routes>
+      )}
+    </>
   );
 };
 
