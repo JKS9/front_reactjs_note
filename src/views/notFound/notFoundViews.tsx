18a2d7cc4930b5f6fcd57a025c3c87409436eb6f
JKS9 <juzans.etienne@gmail.com>
2022-10-18 19:13:01 +0200

    add view NotFound

--- src/views/notFound/notFoundViews.tsx
+++ src/views/notFound/notFoundViews.tsx
@@ -1,5 +1,39 @@
+import {useNavigate} from 'react-router-dom';
+
+import ComponentsBox from '../../components/common/box/componentsBox';
+import ComponentsTypography from '../../components/common/typography/componentsTypography';
+import ComponentsButton from '../../components/common/button/componentsButton';
+
 const NotFoundViews = () => {
-  return <p>notfound</p>;
+  const navigate = useNavigate();
+
+  const handlerRedirecty = () => {
+    navigate('/');
+  };
+
+  return (
+    <ComponentsBox
+      width={400}
+      margin={'6em auto 0 auto'}
+    >
+      <ComponentsTypography
+        variant={'h1'}
+        textAlign={'center'}
+        text={'Note'}
+      />
+      <ComponentsTypography
+        variant={'h4'}
+        textAlign={'center'}
+        text={'PAGE NOT FOUND'}
+      />
+
+      <ComponentsButton
+        submit={() => handlerRedirecty()}
+        disabled={false}
+        text={'Home'}
+      />
+    </ComponentsBox>
+  );
 };
 
 export default NotFoundViews;
