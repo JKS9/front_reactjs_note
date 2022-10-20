8459967a25119c83a4d8194b7e600212dd560c26
JKS9 <juzans.etienne@gmail.com>
2022-10-20 14:32:51 +0200

    update constant color, add contant text

--- src/constants/color/colors.tsx
+++ src/constants/color/colors.tsx
@@ -1,1 +1,2 @@
+export const colorCreate: string = '#2e7d32';
 export const colorError: string = '#d32f2f';
---
+++ src/constants/message/message.tsx
@@ -0,0 +1,18 @@
+/**
+ * TEXT API
+ */
+// API
+export const ERROR_API: string = 'Error, Try again';
+
+/**
+ * TEXT ERROR
+ */
+// Sign UP
+export const ERROR_TEXT_IDENTITY = 'Incorrect username or password';
+
+/**
+ * TEXT CREATE
+ */
+// Sign UP
+export const CREATE_TEXT_SINGUP_IDENTITY =
+  'User created, please log in to access your account.';
